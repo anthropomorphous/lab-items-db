e97e9a7526cfbcdfb66b551fed8b3b4613a97a5f
Mia <lampochka99@bk.ru>
2020-05-15 14:08:07 +0300

    Item model added

---
+++ app/src/main/java/com/example/itemsdb/ItemModel.java
@@ -0,0 +1,62 @@
+package com.example.itemsdb;
+
+import androidx.annotation.NonNull;
+
+public class ItemModel {
+    private int id;
+    private String itemName;
+    private String itemType;
+    private float cost;
+
+    public ItemModel(int id, String itemName, String itemType, float cost) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemType = itemType;
+        this.cost = cost;
+    }
+
+    // some cute method for printing our class contents as string
+    // to logs or alert or whatever
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ItemModel{" +
+                "id=" +
+                ", itemName=" + itemName +
+                ", itemType=" + itemType +
+                ", cost " + cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
